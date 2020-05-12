acc2e928b092f02778aa16b748b2e438e3dc582e
Shawn Sullivan <sullivansm3@gmail.com>
2020-05-12 13:33:15 -0600

    Configure ApolloClient

--- src/index.js
+++ src/index.js
@@ -3,15 +3,29 @@ import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+// Apollo
+import { ApolloProvider } from 'react-apollo'
+import { ApolloClient } from 'apollo-client'
+import { createHttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
 
+// connecting ApolloClient instance with the GraphQL API
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000'
+})
+
+// instantiating ApolloClient 
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+})
+
+// The App is wrapped with the higher-order component ApolloProvider 
+// that gets passed the client as a prop
 ReactDOM.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>,
+  </ApolloProvider>,
   document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+)
 serviceWorker.unregister();
