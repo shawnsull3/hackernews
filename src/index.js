1162d79c74989a08c19d81e91f54e6dee1ec46bf
Shawn Sullivan <sullivansm3@gmail.com>
2020-05-16 13:43:14 -0600

    Implement Authentication for signup & login

--- src/index.js
+++ src/index.js
@@ -9,15 +9,28 @@ import { ApolloProvider } from 'react-apollo'
 import { ApolloClient } from 'apollo-client'
 import { createHttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
+import { setContext } from 'apollo-link-context'
+import { AUTH_TOKEN } from './constants'
 
 // connecting ApolloClient instance with the GraphQL API
 const httpLink = createHttpLink({
   uri: 'http://localhost:4000'
 })
 
+// authentication middleware
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem(AUTH_TOKEN)
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : ''
+    }
+  }
+})
+
 // instantiating ApolloClient 
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache()
 })
 
